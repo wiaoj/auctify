338fbaafe94bb0e7fb5ed6dc686c43a954f50fd5
wiaoj <0.wiaoj@gmail.com>
2025-07-29 02:00:02 +0300

    feat(auction): Implement AuctionGrain for in-memory state management

---
+++ src/Services/AuctionService/Auctify.AuctionService.WebAPI/Grains/AuctionGrain.cs
@@ -0,0 +1,17 @@
+﻿namespace Auctify.AuctionService.WebAPI.Grains;
+public sealed class AuctionGrain : Grain, IAuctionGrain {
+    private decimal _currentPrice = 100; 
+
+    public Task<decimal> GetCurrentPrice() {
+        return Task.FromResult(_currentPrice);
+    }
+
+    public Task<bool> PlaceBid(decimal newPrice) {
+        if (newPrice > _currentPrice) {
+            _currentPrice = newPrice;
+            return Task.FromResult(true);
+        }
+
+        return Task.FromResult(false);
+    }
+}
---
+++ src/Services/AuctionService/Auctify.AuctionService.WebAPI/Grains/IAuctionGrain.cs
@@ -0,0 +1,8 @@
+﻿namespace Auctify.AuctionService.WebAPI.Grains;
+[Alias("Auctify.AuctionService.WebAPI.Grains.IAuctionGrain")]
+public interface IAuctionGrain : IGrainWithStringKey {
+    [Alias(nameof(PlaceBid))]
+    Task<bool> PlaceBid(decimal newPrice);
+    [Alias(nameof(GetCurrentPrice))]
+    Task<decimal> GetCurrentPrice();
+}
