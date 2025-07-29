b1ac8787048f248006dcc5b377dd1bcf5fac5f2d
wiaoj <0.wiaoj@gmail.com>
2025-07-29 03:44:52 +0300

    feat(webapp): Configure HttpClient and add SignalR client package

--- src/WebUIs/Auctify.WebApp/Program.cs
+++ src/WebUIs/Auctify.WebApp/Program.cs
@@ -2,10 +2,22 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Auctify.WebApp;
 
+//var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//await builder.Build().RunAsync();
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// AuctionService API'si için isimlendirilmiþ bir HttpClient ekliyoruz.
+// Adres, AuctionService'inizin çalýþtýðý adres olmalý.
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7164") });
+ 
 
 await builder.Build().RunAsync();
