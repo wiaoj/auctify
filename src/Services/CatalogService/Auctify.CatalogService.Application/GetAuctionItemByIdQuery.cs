eb979ac8fac1aee0d6e41e7c53fd58e7d646f684
wiaoj <0.wiaoj@gmail.com>
2025-07-30 04:51:49 +0300

    feat(catalog): Implement Application layer with initial CQRS query
    Adds the Application layer and defines the first query, GetAuctionItemByIdQuery, to fetch auction item details. This establishes the CQRS pattern for this service.

---
+++ src/Services/CatalogService/Auctify.CatalogService.Application/GetAuctionItemByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace Auctify.CatalogService.Application; 
+public sealed record GetAuctionItemByIdQuery {
+
+}
+
+public sealed record GetAuctionItemByIdQueryResult {
+
+}
+
+public sealed class GetAuctionItemByIdQueryHandler {
+    public ValueTask<GetAuctionItemByIdQueryResult> HandleAsync(GetAuctionItemByIdQuery query, CancellationToken cancellationToken) {
+
+        return ValueTask.FromResult(new GetAuctionItemByIdQueryResult());
+    }
+}
